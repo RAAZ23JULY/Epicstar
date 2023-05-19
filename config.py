d34af499b9641b0852481aa44e13791ba8fc5bbb
SHOKINx2 <134042364+SHOKINx2@users.noreply.github.com>
2023-05-19 16:59:35 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "SNOWY ダ MUSIC")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "𝗦𝗛𝗢𝗞𝗜𝗡✘𝗕𝗔𝗕𝗬«🇮🇳»")
 
 OWNER_ID = list(map(int, getenv("OWNER_ID", "1356469075").split()))
 
@@ -25,8 +25,8 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/SHOKINx2/SHOKINX3")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SNOWY_SUPPORT")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/SNOWY_SUPPORT")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SHOKINXLOG")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/SHOKINXLOG")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
@@ -76,11 +76,11 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/bcf41395bf1d07f4fe068.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/584dc6bae77e012a7ac55.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
-    "https://te.legra.ph/file/bcf41395bf1d07f4fe068.jpg",
+    "https://te.legra.ph/file/584dc6bae77e012a7ac55.jpg",
 )
 
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
@@ -130,9 +130,9 @@ if UPSTREAM_REPO:
 if PING_IMG_URL:
     if PING_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", PING_IMG_URL):
-            PING_IMG_URL = "https://te.legra.ph/file/bcf41395bf1d07f4fe068.jpg"
+            PING_IMG_URL = "https://te.legra.ph/file/584dc6bae77e012a7ac55.jpg"
 
 if START_IMG_URL:
     if START_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = "https://te.legra.ph/file/bcf41395bf1d07f4fe068.jpg"
+            START_IMG_URL = "https://te.legra.ph/file/584dc6bae77e012a7ac55.jpg"
