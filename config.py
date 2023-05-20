d977ca302e969409a67320d84024740e1044909e
SHOKINx2 <134042364+SHOKINx2@users.noreply.github.com>
2023-05-20 20:00:59 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "𝗦𝗛𝗢𝗞𝗜𝗡✘𝗕𝗔𝗕𝗬«🇮🇳»")
 
-OWNER_ID = list(map(int, getenv("OWNER_ID", "6204413512").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6001706215").split()))
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
@@ -25,8 +25,8 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/SHOKINx2/SHOKINX3")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SHOKINXLOG")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/SHOKINXLOG")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SHOKIN_LOG")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/SHOKIN_LOG")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
@@ -80,7 +80,7 @@ START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/584dc6bae77e01
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
-    "https://te.legra.ph/file/584dc6bae77e012a7ac55.jpg",
+    "https://te.legra.ph/file/813069a833703a1167d35.jpg",
 )
 
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
@@ -130,7 +130,7 @@ if UPSTREAM_REPO:
 if PING_IMG_URL:
     if PING_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", PING_IMG_URL):
-            PING_IMG_URL = "https://te.legra.ph/file/584dc6bae77e012a7ac55.jpg"
+            PING_IMG_URL = "https://te.legra.ph/file/813069a833703a1167d35.jpg"
 
 if START_IMG_URL:
     if START_IMG_URL != "assets/Ping.jpeg":
